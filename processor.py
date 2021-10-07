e1a8a56b64ebbe829c6831689327aaa97275f0ec
Jah-On <58399643+Jah-On@users.noreply.github.com>
2021-10-07 04:35:42 -0700

    Add files via upload

--- processor.py
+++ processor.py
@@ -12,8 +12,8 @@ def main():
         if not (lines[line][:lines[line].find(b'_')] == date):
             pings.clear()
             date = lines[line][:lines[line].find(b'_')]
-        if (lines[line][lines[line].find(b'- '):lines[line].find(b'- ') + 1] == b'('):
-            tup = lines[line][lines[line].find(b'- '):].replace(b' ', "")
+        if (lines[line][lines[line].find(b'- ') + 2:lines[line].find(b'- ') + 3] == b'('):
+            tup = lines[line][lines[line].find(b'- ')+1:].replace(b' ', b'')
             speedtests.append(tup[1:len(tup) - 1])
         else:            
             if (date != b''):
@@ -21,14 +21,14 @@ def main():
                 file.write(str(list(eval(b'[' + lines[line][lines[line].find(b'- ') + 2:] + b']')[0].values())).encode("utf-8")[1:-1].replace(b' ', b'') + b'\n')
                 file.close()
 
-        file = open("speed.cvs", "ab")
-        try:
-            file.write(speedtests[0])
-            for i in range(1, len(speedtests)):
-                file.write(b'\n' + speedtests[i])
-        except:
-            pass
-        file.close()
+    file = open("speed.csv", "ab")
+    try:
+        file.write(speedtests[0])
+        for i in range(1, len(speedtests)):
+            file.write(b'\n' + speedtests[i])
+    except:
+        pass
+    file.close()
 
 if __name__ == "__main__":
     main()
